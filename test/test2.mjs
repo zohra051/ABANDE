e40901d71271c8066d01022629b44c9f3479d77f
Eric Ramat <eric.ramat@gmail.com>
2018-01-12 10:16:54 +0100

    Simplify move build

--- test/test2.mjs
+++ test/test2.mjs
@@ -1,13 +1,25 @@
 import OpenXum from '../lib/openxum';
 
-let e = new OpenXum.Invers.Engine(OpenXum.Invers.GameType.STANDARD, OpenXum.Invers.Color.RED);
-let p1 = new OpenXum.Invers.RandomPlayer(OpenXum.Invers.Color.RED, e);
-let p2 = new OpenXum.MCTSPlayer(OpenXum.Invers.Color.YELLOW, e);
-let manager = new OpenXum.AIManager(e, p1, p2);
+let red_win = 0;
+let yellow_win = 0;
 
-while (!e.is_finished()) {
-  manager._run();
+for (let i = 0; i < 20; ++i) {
+  let e = new OpenXum.Invers.Engine(OpenXum.Invers.GameType.STANDARD, OpenXum.Invers.Color.RED);
+  let p1 = new OpenXum.Invers.RandomPlayer(OpenXum.Invers.Color.RED, e);
+  let p2 = new OpenXum.MCTSPlayer(OpenXum.Invers.Color.YELLOW, e);
+  let manager = new OpenXum.AIManager(e, p1, p2);
+
+  while (!e.is_finished()) {
+    manager._run();
+  }
+
+  console.log("Winner is " + (e.winner_is() === OpenXum.Invers.Color.RED ? "red" : "yellow"));
+  if (e.winner_is() === OpenXum.Invers.Color.RED) {
+    red_win++;
+  } else {
+    yellow_win++;
+  }
 }
 
-console.log(manager._all_moves);
-console.log("Winner is " + (e.winner_is() === OpenXum.Invers.Color.RED ? "red" : "yellow"));
+console.log("Yellow wins: " + yellow_win);
+console.log("Red wins: " + red_win);
