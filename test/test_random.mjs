2abe73b5f24438032f5e76b7166d66ab7a40b876
Eric Ramat <eric.ramat@gmail.com>
2018-01-28 10:12:15 +0100

    test: show moves of dvonn game

--- test/test_random.mjs
+++ test/test_random.mjs
@@ -4,10 +4,17 @@ let e = new lib.OpenXum.Dvonn.Engine(lib.OpenXum.Dvonn.GameType.STANDARD, lib.Op
 let p1 = new lib.OpenXum.RandomPlayer(lib.OpenXum.Dvonn.Color.BLACK, e);
 let p2 = new lib.OpenXum.RandomPlayer(lib.OpenXum.Dvonn.Color.WHITE, e);
 let p = p1;
+let moves = [];
 
 while (!e.is_finished()) {
-  e.move(p.move());
+  let move = p.move();
+
+  moves.push(move);
+  e.move(move);
   p = p === p1 ? p2 : p1;
 }
 
 console.log("Winner is " + (e.winner_is() === lib.OpenXum.Dvonn.Color.BLACK ? "black" : "white"));
+for (let index = 0; index < moves.length; ++index) {
+  console.log(moves[index].to_string());
+}
